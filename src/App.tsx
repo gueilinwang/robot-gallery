b94efb406c0f9f7f54713c262d0b736354b053f8
gueilinwang <cool4003213@gmail.com>
2021-07-12 21:34:35 +0800

    將資料從web fetch到component中

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,44 @@ import robotdata from './mockdata/robot.json'
 import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart'
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} alt="logo" className={styles.appLogo} />
-        <h1>Robot List</h1>
+interface IProps {}
+interface IState {
+  robotGallery: any[]
+}
+
+class App extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props)
+    this.state = {
+      robotGallery: [],
+    }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => this.setState({ robotGallery: data }))
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} alt="logo" className={styles.appLogo} />
+          <h1>Robot List</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((data) => (
+            <Robot
+              id={data.id}
+              name={data.name}
+              email={data.email}
+              key={data.id}
+            />
+          ))}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robotdata.map((data) => (
-          <Robot
-            id={data.id}
-            name={data.name}
-            email={data.email}
-            key={data.id}
-          />
-        ))}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
